47c4dae7807bdff2b8370b744025147f988b2b2f
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-25 00:24:10 +0300

    AuthProvide configured

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+"use client"
+import {getProviders, signIn} from "next-auth/react";
+import React, {useState, useEffect} from 'react';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signInUrl: string;
+    callbackUrl: string;
+    signInUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-    return (
-        <div>
-            AuthProviders
-        </div>
-    );
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <button key={i}>{provider.id}</button>
+                ))}
+            </div>
+        )
+    }
 };
 
 export default AuthProviders;
