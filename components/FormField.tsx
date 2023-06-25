25825d4dd23fe27f89d33cdfe6514b4f1d5a1aba
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-26 01:27:01 +0300

    Modal is complete. Project adding form initialized.

---
+++ components/FormField.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+type Props={
+    type?:string;
+    title:string;
+    state:string;
+    placeholder:string;
+    isTextArea?:boolean;
+    setState:(value:string)=>void;
+}
+
+const FormField = ({type,title,state,placeholder,isTextArea,setState}:Props) => {
+    return (
+        <div className="flexStart flex-col w-full gap-4">
+            <label htmlFor="" className="w-full text-gray-100">{title}</label>
+            {isTextArea ? (
+                <textarea/>
+            ): (
+                <input/>
+            )}
+        </div>
+    );
+};
+
+export default FormField;
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,14 +1,23 @@
 "use client"
-import {useCallback,useRef,ReactNode} from "react";
+import React, {useCallback, useRef, ReactNode} from "react";
 import {useRouter} from "next/navigation";
 import Image from "next/image";
 
-const Modal = ({children}:{children:ReactNode}) => {
+const Modal = ({children}: { children: ReactNode }) => {
     const overlay = useRef<HTMLDivElement>(null);
     const wrapper = useRef<HTMLDivElement>(null);
     const router = useRouter();
-    const handleClick = ()=>{};
-    const onDismiss = ()=>{};
+
+    const onDismiss = useCallback(() => {
+        router.push('/');
+    }, [router]);
+
+    const handleClick = useCallback((e: React.MouseEvent) => {
+        if ((e.target === overlay.current) && onDismiss) {
+            onDismiss();
+        }
+    }, [onDismiss, overlay]);
+
     return (
         <div ref={overlay} className="modal" onClick={handleClick}>
             <button type="button" onClick={onDismiss} className="absolute top-4 right-8">
