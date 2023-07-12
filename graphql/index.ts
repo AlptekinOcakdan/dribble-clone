b7501207ee23c1366b736c93999dac7d366651c1
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-12 09:58:31 +0300

    Package updated.

--- graphql/index.ts
+++ graphql/index.ts
@@ -31,11 +31,11 @@ export const updateProjectMutation = `
 `;
 
 export const deleteProjectMutation = `
-    mutation DeleteProject($id: ID!) {
-        projectDelete(by: { id: $id }) {
-            deletedId
-        }
+  mutation DeleteProject($id: ID!) {
+    projectDelete(by: { id: $id }) {
+      deletedId
     }
+  }
 `;
 
 export const createUserMutation = `
@@ -55,88 +55,88 @@ export const createUserMutation = `
 `;
 
 export const projectsQuery = `
-    query getProjects($category: String, $endCursor: String) {
-        projectSearch(first: 8, after: $endCursor, filter: {category: {eq: $category}}) {
-            pageInfo {
-                hasNextPage
-                hasPreviousPage
-                startCursor
-                endCursor
-            }
-            edges {
-                node {
-                    title
-                    githubUrl
-                    description
-                    liveSiteUrl
-                    id
-                    image
-                    category
-                    createdBy {
-                        id
-                        email
-                        name
-                        avatarUrl
-                    }
-                }
-            }
+  query getProjects($category: String, $endcursor: String) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
         }
+      }
     }
+  }
 `;
 
 export const getProjectByIdQuery = `
-    query GetProjectById($id: ID!) {
-        project(by: { id: $id }) {
-            id
-            title
-            description
-            image
-            liveSiteUrl
-            githubUrl
-            category
-            createdBy {
-                id
-                name
-                email
-                avatarUrl   
-            }
-        }
-    }
-`;
-
-export const getUserQuery = `
-    query GetUser($email: String!) {
-        user(by: { email: $email }) {
+  query GetProjectById($id: ID!) {
+    project(by: { id: $id }) {
+      id
+      title
+      description
+      image
+      liveSiteUrl
+      githubUrl
+      category
+      createdBy {
         id
         name
         email
         avatarUrl
-        description
-        githubUrl   
-        linkedinUrl
-        }
+      }
     }
+  }
+`;
+
+export const getUserQuery = `
+  query GetUser($email: String!) {
+    user(by: { email: $email }) {
+      id
+      name
+      email
+      avatarUrl
+      description
+      githubUrl
+      linkedinUrl
+    }
+  }
 `;
 
 export const getProjectsOfUserQuery = `
-    query getUserProjects($id: ID!, $last: Int = 4) {
-        user(by: { id: $id }) {
+  query getUserProjects($id: ID!, $last: Int = 4) {
+    user(by: { id: $id }) {
+      id
+      name
+      email
+      description
+      avatarUrl
+      githubUrl
+      linkedinUrl
+      projects(last: $last) {
+        edges {
+          node {
             id
-            name
-            email
-            description
-            avatarUrl
-            githubUrl
-            linkedinUrl
-            projects(last: $last) {
-                edges {
-                    node {
-                        id
-                        title
-                        image
-                    }
-                }
-            }
+            title
+            image
+          }
         }
+      }
     }
+  }
 `;
