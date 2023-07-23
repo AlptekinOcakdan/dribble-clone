c242a9e5ecde3a45a9de67559d7c8153c70803c8
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-23 17:07:00 +0300

    redeployment

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,6 @@ const Home = async ({ searchParams: { category="Frontend", endcursor } }: Props)
         return (
             <section className="flexStart flex-col paddings">
                 <Categories  />
-
                 <p className="no-result-text text-center">No projects found, go create some first.</p>
             </section>
         )
