21248808c1cde706df01d676fe78e0d97c71968f
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-23 16:39:01 +0300

    Changing back to my codes and making an initial value to category at home page.

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,7 @@
 "use client"
-
-import { getProviders, signIn } from 'next-auth/react';
-import React, { useEffect, useState } from 'react'
-
-import Button from './Button';
+import {getProviders, signIn} from "next-auth/react";
+import React, {useState, useEffect} from 'react';
+import Button from "@/components/Button";
 
 type Provider = {
     id: string;
@@ -12,33 +10,29 @@ type Provider = {
     signinUrl: string;
     callbackUrl: string;
     signinUrlParams?: Record<string, string> | undefined;
-  };
-  
-  type Providers = Record<string, Provider>;
+};
 
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
     const [providers, setProviders] = useState<Providers | null>(null);
-
     useEffect(() => {
         const fetchProviders = async () => {
             const res = await getProviders();
-    
+
             setProviders(res);
         }
-
         fetchProviders();
     }, []);
-
     if (providers) {
         return (
             <div>
                 {Object.values(providers).map((provider: Provider, i) => (
-                    <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
+                    <Button key={i} title="Sign In" handleClick={() => signIn(provider?.id)}/>
                 ))}
             </div>
         )
     }
-}
+};
 
-export default AuthProviders
+export default AuthProviders;
--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  }
+    images:{
+        domains:['lh3.googleusercontent.com','res.cloudinary.com']
+    },
+    experimental:{
+        serverComponentsExternalPackages:['cloudinary','graphql-request']
+    }
 }
 
 module.exports = nextConfig
